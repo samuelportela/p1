6aad1294ac8a6f9b94b255cd4e89631f7ad02a2a
Samuel Portela <negociosnainternet@gmail.com>
2011-11-19 01:14:30 -0300

    created new rails app with "rails new <APP> -T" command (the -T option was provided to skip Test::Unit files)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P1::Application.config.secret_token = 'a9f96c65751f0693b1f576568853f064d88a670989934ea2cb13e08d60b22ca4812a7d49f2f5ef05f3319342afdf1c8d1946010e3fa106d8ae0e28a08f3521de'
