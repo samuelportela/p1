82920f145689ffffaa5d0f6a40925ef8a9ab57fe
Samuel Portela <negociosnainternet@gmail.com>
2011-11-19 13:58:38 -0300

    refactoring test to make it expect a specific content

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -4,6 +4,7 @@ describe "Products" do
   describe "GET /products" do
     it "should display some products" do
       visit products_path
+      page.should have_content 'magic mouse'
     end
   end
 end
