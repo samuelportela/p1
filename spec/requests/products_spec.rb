9676efff46ee13ee8b83c8b53a697e9a78de4b80
Samuel Portela <negociosnainternet@gmail.com>
2011-11-20 14:06:52 -0300

    changing Products#index implementation to make test pass

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe "Products" do
   describe "GET /products" do
     it "should display some products" do
+      Product.create :name => 'magic mouse'
+      
       visit products_path
       page.should have_content 'magic mouse'
     end
