337953ea114e8f47a806733aba1bd45225b1ea95
Samuel Portela <negociosnainternet@gmail.com>
2012-03-19 23:03:52 -0300

    auction_cover helper refactored to display missing image (without link) when auction object is nil. the code has been moved from HomeHelper to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  
+  def auction_cover(auction, size = :small)
+    if auction
+      cover_list = cover_photos(auction.product.photos)
+
+      if cover_list && cover_list.any?
+        raw link_to(image_tag(cover_list.first.file(size)), auction_path(auction))
+      else
+        raw link_to(image_tag(Photo.new.file(size)), auction_path(auction))
+      end
+      
+    else
+      raw image_tag(Photo.new.file(size))
+    end
+  end
+  
+  def cover_photos(photos)
+    photos.where(:is_cover => true)
+  end
+  
 end
