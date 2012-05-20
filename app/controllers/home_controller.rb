86fce297da110325a7f7e1e697a7ff6728e1b228
Samuel Portela <negociosnainternet@gmail.com>
2012-05-20 15:32:45 -0300

    end price and display name updated through jquery queue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < BaseController
   respond_to :html
   
   def index
-    @auctions = Auction.where(:is_active => true)
+    @auctions = Auction.where(:is_active => true).order(:id)
     respond_with @auctions
   end
 end
